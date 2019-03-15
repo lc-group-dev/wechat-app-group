25b3076b7f9d998317148d0ea7a9e064f514d6ce
e <luthernpu@gmail.com>
2019-03-15 11:04:14 +0800

    d

--- src/config.js
+++ src/config.js
@@ -1,2 +1,8 @@
 export const APPID = 'wxf1ab6b7a4fe6f409'
 export const APPSECRET = 'f701618c180956ba4b58e038e08d4170'
+
+export const TOPBARTYPE = {
+  OWNQUESTION: 0,
+  CARDDAYS: 1,
+  CONTINUESCARDDAYS: 2
+}
